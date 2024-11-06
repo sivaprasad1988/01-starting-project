48504cb7c83c8f2e2388f07fb2302f7b2db9524e
Siva <sivaprasad.s88@gmail.com>
2024-11-06 16:47:55 +0100

    [TASK] Create a new component with Tab

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import componentsImage from "./assets/components.png";
 import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header";
 import CoreComponents from "./components/CoreComponents";
+import TabButton from "./components/TabButton";
 
 function App() {
   console.log(CORE_CONCEPTS);
@@ -18,6 +19,12 @@ function App() {
             <CoreComponents {...CORE_CONCEPTS[2]} />
             <CoreComponents {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Example</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+          </menu>
         </section>
       </main>
     </div>
